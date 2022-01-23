02311fa2e4130c82d37c9c23ca809371dbcda78d
Dayalan <85922079+dayalan22102001@users.noreply.github.com>
2022-01-23 18:38:34 +0530

    Add files via upload

---
+++ sendemails.py
@@ -0,0 +1,35 @@
+import smtplib
+import ssl #secure socket layer
+from email.message import EmailMessage
+
+subject = "Email From Python"
+body = "Some cool mail tranfer using Python"
+sender_email = "dayalandsofficial2210@gmail.com"
+receiver_email = "dayalandsofficial2210@gmail.com"
+password = input("Enter a password: ")
+
+message = EmailMessage()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+
+html = f"""
+<html>
+    <body>
+        <h1>{subject}</h1>
+        <p>{body}</p>
+    </body>
+</html>
+"""
+
+message.add_alternative(html, subtype="html")
+
+context = ssl.create_default_context()
+
+print("Sending Email!")
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message.as_string())
+
+print("Success")
